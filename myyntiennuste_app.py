71b7b0dee2f923ab404c6cf25d94df1aa42ab4be
krla80 <kristina.lamberg@live.com>
2025-07-04 17:35:42 +0300

    Update myyntiennuste_app.py

--- myyntiennuste_app.py
+++ myyntiennuste_app.py
@@ -337,7 +337,7 @@ with tab3:
             st.error("Syötä kelvollinen numero nettopalkkatavoitteeksi.")
             tavoitepalkka = 0
 
-    myyntikuilu = (kulut_yhteensa+tavoitepalkka/(1-vero_prosentti)-total_sopimus)
+    myyntikuilu = (kulut_yhteensa+tavoitepalkka*12/(1-vero_prosentti)-total_sopimus)
 
 
 # Tulokset näkyviin

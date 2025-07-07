de3cdaead20259fa301ef6a010d33f1f07be0cdd
krla80 <kristina.lamberg@live.com>
2025-07-07 14:42:54 +0300

    Update myyntiennuste_app.py

--- myyntiennuste_app.py
+++ myyntiennuste_app.py
@@ -360,7 +360,7 @@ with tab3:
         kokonaismyynti = total_sopimus / 12
         bruttopalkka = kokonaismyynti - (kulut_yhteensa / 12)
         verot = bruttopalkka * (vero_prosentti / 100) if bruttopalkka > 0 else 0
-        nettopalkka = bruttopalkka - verot*1,019 if bruttopalkka > 0 else 0
+        nettopalkka = bruttopalkka - verot*1.019 if bruttopalkka > 0 else 0
         myyntikuilu = total_sopimus - (kulut_yhteensa + tavoitepalkka * 12 / (1 - vero_prosentti / 100))
         
         st.markdown(f"**Nettopalkka-arvio:** {nettopalkka:.2f} € / kk")

1e6a8a23f2e2d47ecfdf7232f24d971ef1609dc8
krla80 <kristina.lamberg@live.com>
2025-07-07 13:12:40 +0300

    Update myyntiennuste_app.py

--- myyntiennuste_app.py
+++ myyntiennuste_app.py
@@ -311,7 +311,7 @@ with tab3:
 
         st.form_submit_button("Tallenna kulut")
         st.session_state.asiakkaat_palkkaennuste = kulutiedot
-            save_data(PALKKAENNUSTE_FILE, kulutiedot)
+        save_data(PALKKAENNUSTE_FILE, kulutiedot)
             st.success("Kulut tallennettu onnistuneesti.")
 
     st.subheader("Tallennetut kulut:")

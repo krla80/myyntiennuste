48c5d86fab5ebfb5cc509540915c1d9f22fcaab1
krla80 <kristina.lamberg@live.com>
2025-07-08 19:00:19 +0300

    Update Uusi_myyntiennuste_app.py

--- Uusi_myyntiennuste_app.py
+++ Uusi_myyntiennuste_app.py
@@ -446,7 +446,7 @@ with tab3:
         vero_prosentti = int(st.session_state.get("veroprosentti", 25))
         kokonaismyynti = total_sopimus / 12
         bruttopalkka = kokonaismyynti - (kulut_yhteensa / 12)
-        verot = bruttopalkka * (vero_prosentti / 100) if bruttopalkka > 0 else 0
+        verot = bruttopalkka * (vero_prosentti / 100)if bruttopalkka > 0 else 0
         nettopalkka = bruttopalkka - verot* if bruttopalkka > 0 else 0
         myyntikuilu = total_sopimus - (kulut_yhteensa + tavoitepalkka * 12 / (1 - vero_prosentti / 100))
             

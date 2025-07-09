decb990e79cbe6a9a1ec217770d1d68538053ef3
krla80 <kristina.lamberg@live.com>
2025-07-09 14:58:29 +0300

    Update muokattu koodi_app.py

--- muokattu koodi_app.py
+++ muokattu koodi_app.py
@@ -359,7 +359,7 @@ with tab2:
 # Tulokset näkyviin
     st.markdown(f"<h4>Liikevaihto kuukaudessa perustuen toteutuneeseen myyntiin: {kokonaismyynti:.2f} €</h4>", unsafe_allow_html=True)
     st.markdown(f"<h2 style='color:#4EA72E;'>Arvioitu nettopalkka kuukaudessa kulujen ja verojen jälkeen: {nettopalkka:.2f} €</h2>", unsafe_allow_html=True)
-    #st.markdown(f"<h2 style='color:red;'>Näin paljon sinun pitää myydä jo tehtyjen sopimusten tällä tilikaudella lisäksi saavuttaaksesi tavoitepalkkasi: {myyntikuilu:.0f} €</h2>", unsafe_allow_html=True)
+    st.markdown(f"<h2 style='color:red;'>Näin paljon sinun pitää myydä jo tehtyjen sopimusten tällä tilikaudella lisäksi saavuttaaksesi tavoitepalkkasi: {myyntikuilu:.0f} €</h2>", unsafe_allow_html=True)
     #st.markdown(f"<h4>Arvioitu bruttopalkka kuukaudessa: {bruttopalkka:.2f} €</h4>", unsafe_allow_html=True)
 
 # Ehdollinen palaute tavoitepalkasta
